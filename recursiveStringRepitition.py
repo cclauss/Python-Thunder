b7f099a0fe31dd0a2daf25dbc07ca64d8bde9088
Christian Clauss <cclauss@me.com>
2020-10-03 23:34:19 +0200

    Fix Python syntax error in recursiveStringRepitition.py

--- recursiveStringRepitition.py
+++ recursiveStringRepitition.py
@@ -2,7 +2,7 @@
     Problem Task : This program creates a recursive function that takes two parameters, string txt and number n, and repeats the string n number of times
     Problem Link : https://edabit.com/challenge/QKmETue6fMTdcB8Rq
 '''
-*/
+
 def recursiveStringRepetition(txt,n):
   if(n<=0):
     return ''
